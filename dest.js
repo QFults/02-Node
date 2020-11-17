e3aebb33fb9f751c332179f55c9c604a153c513b
Quinton Fults <quinton.fults.work@gmail.com>
2020-11-17 13:57:28 -0800

    code

---
+++ dest.js
@@ -0,0 +1,23 @@
+// let dog = {
+//   name: 'Beef',
+//   age: 2,
+//   breed: 'chihuahua',
+//   puppy: {
+//     name: 'Chicken',
+//     age: 1,
+//     breed: 'chihuahua'
+//   }
+// }
+
+// let { name: dogName, age: dogAge, breed: dogBreed } = dog
+// let { puppy: { name: puppyName } } = dog
+
+// console.log(puppyName)
+
+// let nums = [1, 2, 3, 4, 5]
+
+// let [you, can, name, these, whatever] = nums
+// let [ , , third, , fifth] = nums
+
+// console.log(third, fifth)
+// console.log(a, b, c, d, e)
---
+++ movie.js
@@ -0,0 +1,23 @@
+// const inquirer = require('inquirer')
+// const axios = require('axios')
+
+// inquirer.prompt([
+//   {
+//     type: 'input',
+//     name: 'title',
+//     message: 'What is your favorite movie?'
+//   }
+// ])
+//   .then(({ title }) => axios.get(`http://www.omdbapi.com/?apikey=trilogy&t=${title}`))
+//   .then(({ data: movie }) => {
+//     console.log(`
+//          --------------------
+//          ${movie.Title}
+//          Directed by ${movie.Director}
+//          Released in ${movie.Year}
+//          ${movie.Plot}
+//          --------------------
+//          `)
+//   })
+//   .catch(err => console.log(err))
+  
